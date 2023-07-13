3ee7d200aedfd7b2498eb85116e13842c951effa
DivingStu <139291856+DivingStu@users.noreply.github.com>
2023-07-13 17:25:30 +1200

    Update streamlit_app.py
    tidy typo fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
-from urllib.error inport URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')
